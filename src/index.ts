df1756b2b5ead0c20b03e71b652674764bba3712
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-11-14 19:12:12 -0300

    FIX: Validação para não criar um usuário com o cpf repetido

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,53 @@ app.use(express.json())
 
 app.use(cors())
 
-// criar usuário:
+// listando todos os  usuário:
 app.get("/users",(req:Request, res:Response)=>{
      clientes.map((user)=>{
         return user
      })
      res.status(200).send(clientes)
 })
+
+// criando um novo usuário:
+app.post("/users", (req: Request, res: Response)=>{
+    const {nome, cpf, nascimento,saldo} = req.body
+    
+    if(!nome && !cpf && !nascimento && !saldo){
+        return res.status(422).send("Insira todos os parâmetros necessários: Nome, CPF, Data de Nascimento e Saldo.")
+    }else if (!nome){
+        return res.status(422).send("Insira o nome do usuário")
+    }else if (!cpf){
+        return res.status(422).send("Insira o cpf do usuário")
+    }else if (!nascimento){
+        return res.status(422).send("Insira o nascimento do usuário")
+    }
+    
+    const cpfValido= clientes.find((cliente)=>{
+        return cliente.cpf === cpf
+    })
+    
+    
+    if(cpfValido){
+        return res.status(400).send("CPF já está sendo utilizado em outra conta.")
+    }   
+    
+    let novoCliente = {
+        nome, cpf, nascimento, saldo
+    }
+    
+    clientes.push(novoCliente)
+    res.status(201).send(clientes)
+
+
+
+
+
+
+
+
+})
+
 
 app.listen(3003, () => {
     console.log("Servidor rodando na porta http://localhost:3003");
